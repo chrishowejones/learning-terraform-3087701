82df61e0f3050c3f6d35664809b2fbf939c42721
Chris Howe-Jones <chris.howejones@gmail.com>
2024-06-26 17:12:35 +0100

    Try and fix target groups arns in autoscaling

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ module "blog-autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = [module.blog_alb.target_groups[0].arn]
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
@@ -60,7 +60,7 @@ module "blog_alb" {
   security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
-    ex-http-https-redirect = {
+    ex-http = {
       port     = 80
       protocol = "HTTP"
       forward = {
